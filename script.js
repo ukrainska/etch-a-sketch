075f756b28de2e77523870421543eac1b56a9e39
ukrainska <215995465+ukrainska@users.noreply.github.com>
2025-09-19 17:09:49 +0900

    Set boundaries to the input to be less than 100

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".grid");
+const button = document.getElementById("grid_size");
 const containerSize = 16;
-const gridSize = containerSize * containerSize;
+let gridSize = containerSize * containerSize;
 
 // function that adds 16 new divs to a grid class
 function addNewDivs() {
@@ -15,8 +16,23 @@ function addNewDivs() {
     }
 }
 
-function setGridSize (size) {
-    gridSize = size * size;
+function setGridSize () {
+    let answer = prompt("What size of the grid you prefer?");
+    console.log("Grid size set to:", gridSize);
+    if (isNaN(answer)) {
+        alert("Please enter a valid number");
+        return;
+    }
+
+    if (answer < 100) {
+        gridSize = answer * answer; 
+        console.log("Grid size set to:", gridSize);
+        addNewDivs();
+    } else {
+        alert("The number should be less than 100");
+    }
 } 
+
+button.addEventListener("click", setGridSize);
 
 addNewDivs();

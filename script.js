d252e4e820fb2f71d65730647465d3944466a98a
ukrainska <215995465+ukrainska@users.noreply.github.com>
2025-09-19 16:43:00 +0900

    Add a function that sets the grid's size

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const container = document.querySelector(".grid");
 const containerSize = 16;
+const gridSize = containerSize * containerSize;
 
 // function that adds 16 new divs to a grid class
 function addNewDivs() {
     if (container) {
-        for (let i = 0; i < containerSize * containerSize; i++) {
+        for (let i = 0; i < gridSize; i++) {
             const newDiv = document.createElement("div");
             newDiv.classList.add("grid-child");
-            newDiv.innerHTML = i+1;
             container.appendChild(newDiv);
         }
     } else {
         console.error("The parent div was not found");
     }
 }
+
+function setGridSize (size) {
+    gridSize = size * size;
+} 
 
 addNewDivs();

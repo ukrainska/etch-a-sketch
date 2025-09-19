2ea64fa5df7170bae4c70bd6ea830d720cc1565f
ukrainska <215995465+ukrainska@users.noreply.github.com>
2025-09-19 15:43:39 +0900

    Create a function that adds 16 new divs to a grid class

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const container = document.querySelector(".grid");
+
+// function that adds 16 new divs to a grid class
+function addNewDivs() {
+    if (container) {
+        for (let i = 0; i < 16; i++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("grid-child");
+            container.appendChild(newDiv);
+        }
+    } else {
+        console.error("The parent div was not found");
+    }
+}
+addNewDivs();
